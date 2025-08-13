4cf254bd3fa79f68c488f5b7753b725950a75cc9
Jiewen Hu <13383046+yumoumou-hu@user.noreply.gitee.com>
2025-08-13 14:28:58 +0800

    短链接分组 -- 新增分组接口 -- 添加分组排序默认字段

--- admin/src/main/java/com/yum/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/yum/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -34,6 +34,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         GroupDO groupDO = GroupDO.builder()
                 .gid(gid)
                 .name(groupName)
+                .sortOrder(0)
                 .build();
         baseMapper.insert(groupDO);
     }
