91666b1473c002e19e167f9a19ffa06d6e5fb14b
Jiewen Hu <13383046+yumoumou-hu@user.noreply.gitee.com>
2025-08-16 14:56:42 +0800

    短链接库t_link分库分表

---
+++ project/src/test/java/com/yum/shortlink/project/test/UserTableShardingTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yum.shortlink.project.test;
+
+public class UserTableShardingTest {
+
+    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+
+                          "`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "`domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
+            "`short_uri` varchar(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '短链接',\n" +
+            "`full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "`origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
+            "`click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
+            "`gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "`enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：启用 1：未启用',\n" +
+            "`created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：接口 1：控制台',\n" +
+            "`valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
+            "`valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
+            "`describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
+            "`create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "`update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "`del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
+            "PRIMARY KEY (`id`),\n" +
+            "UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
+            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.printf((SQL) + "%n", i);
+        }
+    }
+}
