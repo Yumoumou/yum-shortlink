f75755ef6e73fc0b404a38d7cd43ba2dcb35894c
Jiewen Hu <13383046+yumoumou-hu@user.noreply.gitee.com>
2025-08-17 12:01:18 +0800

    feature: t_group分组分表

--- project/src/test/java/com/yum/shortlink/project/test/UserTableShardingTest.java
+++ project/src/test/java/com/yum/shortlink/project/test/UserTableShardingTest.java
@@ -19,27 +19,18 @@ package com.yum.shortlink.project.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
-
-                          "`id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "`domain` varchar(128) DEFAULT NULL COMMENT '域名',\n" +
-            "`short_uri` varchar(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '短链接',\n" +
-            "`full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
-            "`origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',\n" +
-            "`click_num` int(11) DEFAULT 0 COMMENT '点击量',\n" +
-            "`gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
-            "`favicon` varchar(256) DEFAULT NULL COMMENT '网站图标',\n" +
-            "`enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：启用 1：未启用',\n" +
-            "`created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：接口 1：控制台',\n" +
-            "`valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0：永久有效 1：用户自定义',\n" +
-            "`valid_date` datetime DEFAULT NULL COMMENT '有效期',\n" +
-            "`describe` varchar(1024) DEFAULT NULL COMMENT '描述',\n" +
-            "`create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
-            "`update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
-            "`del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',\n" +
-            "PRIMARY KEY (`id`),\n" +
-            "UNIQUE KEY `idx_unique_full_short_url` (`full_short_url`) USING BTREE\n" +
-            ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+    public static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
+        "`id` bigint(20) not null auto_increment comment 'ID',\n" +
+        "`gid` varchar(32)  null comment '分组标识',\n" +
+        "`name` varchar(64)  null comment '分组名称',\n" +
+        "`username` varchar(256) null comment '创建分组用户名',\n" +
+        "`sort_order` int(3) null comment '分组排序',\n" +
+        "`create_time` datetime null comment '创建时间',\n" +
+        "`update_time` datetime null comment '修改时间',\n" +
+        "`del_flag` tinyint(1) null comment '删除标识 0：未删除 1：已删除',\n" +
+        "PRIMARY KEY (`id`),\n" +
+        "UNIQUE KEY `idx_unique_username_gid` (`gid`, `username`) USING BTREE\n" +
+        ") ENGINE=InnoDB charset = utf8mb4;";
 
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
